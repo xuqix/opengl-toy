5e35ceb71897d4a2e916573d97a0ca361019f5b0
Xu Qi <helloassemble@gmail.com>
2016-09-20 17:29:34 +0800

    完善Shader操作

--- include/Shader.h
+++ include/Shader.h
@@ -2,13 +2,38 @@
 #define SHADER_H
 
 #include <string>
+#include "mathlib.h"
+
+class Texture;
 
 class Shader
 {
 public:
+    enum
+    {
+        VERTEX_POSITION,
+        VERTEX_COLOR,
+        VERTEX_TEXCOORD
+    };
+
+    enum class BuiltinShader
+    {
+        SHADER_POSITION_COLOR,
+        SHADER_POSITION_COLOR_NO_MVP,
+        SHADER_POSITION_COLOR_TEXTURE,
+        SHADER_POSITION_COLOR_TEXTURE_NO_MVP
+    };
+
+    static const char* UNIFORM_M_MATRIX;
+    static const char* UNIFORM_V_MATRIX;
+    static const char* UNIFORM_P_MATRIX;
+    static const char* UNIFORM_TEXTURE0;
+
     Shader();
 
     ~Shader();
+
+    bool initWithBuiltin(BuiltinShader shaderType);
 
     bool initWithFile(const std::string &vertexPath, const std::string &fragmentPath);
 
@@ -16,6 +41,32 @@ public:
 
     // Uses the current shader
     void use();
+
+    int object() const;
+
+    int getUniformLocation(const std::string &name);
+
+    void setUniformValue(const std::string& uniformName, Texture *texture);
+
+    void setUniformValue(const std::string& uniformName, int value);
+
+    void setUniformValue(const std::string& uniformName, float value);
+
+    void setUniformValue(const std::string& uniformName, const glm::Vec2& value);
+
+    void setUniformValue(const std::string& uniformName, const glm::Vec3& value);
+
+    void setUniformValue(const std::string& uniformName, const glm::Vec4& value);
+
+    void setUniformValue(const std::string& uniformName, const glm::Mat4& value);
+
+    void setUniformValuePointer(const std::string& uniformName, ssize_t size, const float* pointer);
+
+    void setUniformValuePointer(const std::string& uniformName, ssize_t size, const glm::Vec2* pointer);
+
+    void setUniformValuePointer(const std::string& uniformName, ssize_t size, const glm::Vec3* pointer);
+
+    void setUniformValuePointer(const std::string& uniformName, ssize_t size, const glm::Vec4* pointer);
 
 private:
     Shader(const Shader&) = delete;
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -5,7 +5,15 @@
 #include <sstream>
 #include <assert.h>
 
+#include "Texture.h"
 #include "GL.h"
+
+#include "Shader.ipp"
+
+const char* Shader::UNIFORM_M_MATRIX = "model";
+const char* Shader::UNIFORM_V_MATRIX = "view";
+const char* Shader::UNIFORM_P_MATRIX = "projection";
+const char* Shader::UNIFORM_TEXTURE0 = "texture0";
 
 struct Shader::PrivateData
 {
@@ -17,6 +25,25 @@ Shader::Shader():_data(new Shader::PrivateData()) {}
 Shader::~Shader()
 {
     delete _data;
+}
+
+bool Shader::initWithBuiltin(BuiltinShader shaderType)
+{
+    switch(shaderType)
+    {
+        case BuiltinShader::SHADER_POSITION_COLOR:
+            return initWithString(s_vertexPositionColor, s_fragmentPositionColor);
+
+        case BuiltinShader::SHADER_POSITION_COLOR_TEXTURE:
+            return initWithString(s_vertexPositionColorTexture, s_fragmentPositionColorTexture);
+
+        case BuiltinShader::SHADER_POSITION_COLOR_NO_MVP:
+            return initWithString(s_vertexPositionColorNoMVP, s_fragmentPositionColorNoMVP);
+
+        case BuiltinShader::SHADER_POSITION_COLOR_TEXTURE_NO_MVP:
+            return initWithString(s_vertexPositionColorTextureNoMVP, s_fragmentPositionColorTextureNoMVP);
+    }
+    return false;
 }
 
 bool Shader::initWithFile(const std::string &vertexPath, const std::string &fragmentPath)
@@ -104,3 +131,70 @@ void Shader::use()
     assert(_data->program != 0);
     glUseProgram(_data->program);
 }
+
+int Shader::object() const
+{
+    return (int)_data->program;
+}
+
+int Shader::getUniformLocation(const std::string & name)
+{
+    return (int)glGetUniformLocation((GLuint)_data->program, name.c_str());
+}
+
+void Shader::setUniformValue(const std::string & uniformName, int value)
+{
+    glUniform1i(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), value);
+}
+
+void Shader::setUniformValue(const std::string & uniformName, float value)
+{
+    glUniform1f(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), value);
+}
+
+void Shader::setUniformValue(const std::string & uniformName, const glm::Vec2 & value)
+{
+    glUniform2f(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), value.x, value.y);
+}
+
+void Shader::setUniformValue(const std::string & uniformName, const glm::Vec3 & value)
+{
+    glUniform3f(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), value.x, value.y, value.z);
+}
+
+void Shader::setUniformValue(const std::string & uniformName, const glm::Vec4 & value)
+{
+    glUniform4f(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), value.x, value.y, value.z, value.w);
+}
+
+void Shader::setUniformValuePointer(const std::string & uniformName, ssize_t size, const float * pointer)
+{
+    glUniform1fv(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), size, pointer);
+}
+
+void Shader::setUniformValuePointer(const std::string & uniformName, ssize_t size, const glm::Vec2 * pointer)
+{
+    glUniform2fv(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), size, glm::value_ptr(*pointer));
+}
+
+void Shader::setUniformValuePointer(const std::string & uniformName, ssize_t size, const glm::Vec3 * pointer)
+{
+    glUniform3fv(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), size, glm::value_ptr(*pointer));
+}
+
+void Shader::setUniformValuePointer(const std::string & uniformName, ssize_t size, const glm::Vec4 * pointer)
+{
+    glUniform4fv(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), size, glm::value_ptr(*pointer));
+}
+
+void Shader::setUniformValue(const std::string & uniformName, const glm::Mat4 & value)
+{
+    glUniformMatrix4fv(glGetUniformLocation((GLuint)_data->program, uniformName.c_str()), 1, GL_FALSE, glm::value_ptr(value));
+}
+
+void Shader::setUniformValue(const std::string & uniformName, Texture * texture)
+{
+    setUniformValue(uniformName.c_str(), (int)0);
+    texture->active();
+}
+
