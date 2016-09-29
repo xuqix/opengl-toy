a532b2301f6472a6d99f11d58bd1517cda4530d3
Xu Qi <helloassemble@gmail.com>
2016-09-29 16:27:48 +0800

    添加测试

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,120 @@
 #include <iostream>
-#include "glm/glm.hpp"
+
+#include "GL.h"
 #include "SOIL.h"
+#include "mathlib.h"
+
+#include "DebugHelper.h"
+
+#include "GLWindow.h"
+#include "Camera.h"
+#include "Shader.h"
+#include "Texture.h"
+#include "Image.h"
+#include "Utils.h"
 
 using namespace std;
+using namespace Utils;
 
 int main()
 {
-    cout << "test" << endl;
+    int width = 480, height = 300;
+
+    GLWindow window(width, height, "my glwindow");
+
+    Shader shader;
+    //shader.initWithBuiltin(Shader::BuiltinShader::SHADER_POSITION_COLOR_TEXTURE_NO_MVP);
+    shader.initWithBuiltin(Shader::BuiltinShader::SHADER_POSITION_COLOR_TEXTURE);
+
+    Image img("resource/pic1.png");
+    if(!img.valid()) return 0;
+    Texture tex(&img);
+
+    GLuint indices[] = {
+        0, 1, 2,
+        1, 2, 3
+    };
+    GLfloat vert[] = {
+        -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f,   0.0f, 1.0f, //bot left
+        0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f,   1.0f, 1.0f, //bot right
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f,   0.0f, 0.0f, //top left
+        0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f,   1.0f, 0.0f, //top right
+    };
+
+    GLuint vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vert), vert, GL_STATIC_DRAW);
+
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);
+
+    float amendW=0, amendS=0, amendA=0, amendD=0;
+    window.setKeyEventCallback([=](GLWindow *win, KeyEvent e){
+        switch(e.keyCode)
+        {
+            case KeyEvent::KeyCode::KEY_W:
+                break;
+            case KeyEvent::KeyCode::KEY_S:
+                break;
+            case KeyEvent::KeyCode::KEY_A:
+                break;
+            case KeyEvent::KeyCode::KEY_D:
+                break;
+        }
+    });
+
+    float amendX = 0.0f, amendY = 0.0f, speed = 0.01f;
+    Camera camera(glm::Vec3(0, 0, 2.0f));
+    while(!window.shouldClose())
+    {
+        if(window.getKeyState(KeyEvent::KeyCode::KEY_W))
+            amendY += speed;
+        else if(window.getKeyState(KeyEvent::KeyCode::KEY_S))
+            amendY -= speed;
+        else if(window.getKeyState(KeyEvent::KeyCode::KEY_A))
+            amendX -= speed;
+        else if(window.getKeyState(KeyEvent::KeyCode::KEY_D))
+            amendX += speed;
+
+        window.clear();
+
+        shader.use();
+        shader.setUniformValue(Shader::UNIFORM_TEXTURE0, &tex);
+        glm::mat4 model, view, projection;
+        model = glm::translate(model, glm::vec3(amendX, amendY, 0));
+        //model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0, 0, 1));
+        shader.setUniformValue(Shader::UNIFORM_M_MATRIX, model);
+
+        camera.setRotation(glm::vec3(10.0f, 10.0f, 0.0f));
+        view = camera.getViewMatrix();
+        shader.setUniformValue(Shader::UNIFORM_V_MATRIX, view);
+
+        //camera.setOrtho(-1, 1, -1, 1, 0, 4);
+        camera.setPerspective(45.0f, 1.0f, 0.1f, 100.0f);
+        projection = camera.getProjectionMatrix();
+        shader.setUniformValue(Shader::UNIFORM_P_MATRIX, projection);
+
+        glBindVertexArray(vao);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        window.swapBuffers();
+        window.pollEvents();
+    }
     return 0;
 }
 
